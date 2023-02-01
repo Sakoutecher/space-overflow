e472dc635d8471fb56d2954bcc10d2fa28e50279
hcampos <hcampospro@gmail.com>
2023-02-01 14:23:28 +0100

    :sparkles: Add homepage route

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuestionController {
+    #[Route('/')]
+    public function homepage(): Response {
+        return new Response('<h1>Hello classroom 🌐</h1>');
+    }
+}
