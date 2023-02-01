e08e891f2734da48b99314e5c2d9d68dbcf80111
hcampos <hcampospro@gmail.com>
2023-02-01 14:28:23 +0100

    :sparkles: Add trou noir route

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -8,6 +8,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class QuestionController {
     #[Route('/')]
     public function homepage(): Response {
-        return new Response('<h1>Hello classroom 🌐</h1>');
+        return new Response("<h1>Hello classroom 🌐.</h1>");
+    }
+
+    #[Route('/comment-sortir-dun-trou-noir')]
+    public function trounoir(): Response {
+        return new Response("<h1>J'avoue je ne sais pas du tout 🫤.</h1>");
     }
 }
