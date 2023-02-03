2429e6bd24839217752f9d932d8314e36a0832c3
hcampos <hcampospro@gmail.com>
2023-02-03 11:23:48 +0100

    ➕ Add markdown bundle

--- src/Controller/AnswersController.php
+++ src/Controller/AnswersController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AnswersController extends AbstractController {
-    #[Route('/answers/{id<\d+>}/vote/{direction<up|down>}', name: 'app_answers_vote', methods: ['POST'])]
-    public function vote(string $direction): Response {
+    #[Route('/answers/{id<\d+>}/vote/{direction<up|down>}', name: 'app_answers_vote', methods: ['POST', 'GET'])]
+    public function vote(string $direction, LoggerInterface $logger): Response {
+        $logger->info("Quelqu'un a voté");
         if ($direction === 'up') {
             $value = random_int(0, 100);
         } else if ($direction === 'down') {
--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -7,11 +7,13 @@ use App\DTO\Question;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class QuestionController extends AbstractController {
     #[Route('/', name: 'app_question_homepage')]
-    public function homepage(): Response {
-        return $this->render('question/homepage.html.twig');
+    public function homepage(Environment $twig): Response {
+        return new Response($twig->render('question/homepage.html.twig'));
+        // return $this->render('question/homepage.html.twig');
     }
 
     #[Route('/questions/{slug}', name: 'app_question_show')]
