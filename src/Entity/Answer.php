97d659350788755fcc9c9b2fed81363ff25cb235
hcampos <hcampospro@gmail.com>
2023-02-07 16:31:09 +0100

    🧑‍💻 Do a entity with make

---
+++ src/Entity/Answer.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AnswerRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AnswerRepository::class)]
+class Answer
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'answers')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Question $question = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getQuestion(): ?Question
+    {
+        return $this->question;
+    }
+
+    public function setQuestion(?Question $question): self
+    {
+        $this->question = $question;
+
+        return $this;
+    }
+}
--- src/Entity/Question.php
+++ src/Entity/Question.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\QuestionRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -31,74 +33,112 @@ class Question {
     #[ORM\Column(type: 'datetime', nullable: true)]
     private ?\DateTime $askedAt = null;
 
+    #[ORM\OneToMany(mappedBy: 'question', targetEntity: Answer::class, orphanRemoval: true)]
+    private Collection $answers;
+
+    public function __construct()
+    {
+        $this->answers = new ArrayCollection();
+    }
+
 	/**
 	 * @return int
 	 */
 	public function getId(): int {
-		return $this->id;
-	}
+                        		return $this->id;
+                        	}
 	
 	/**
 	 * @return string
 	 */
 	public function getSlug(): string {
-		return $this->slug;
-	}
+                        		return $this->slug;
+                        	}
 	
 	/**
 	 * @param string $slug 
 	 * @return self
 	 */
 	public function setSlug(string $slug): self {
-		$this->slug = $slug;
-		return $this;
-	}
+                        		$this->slug = $slug;
+                        		return $this;
+                        	}
 	
 	/**
 	 * @return string
 	 */
 	public function getTitle(): string {
-		return $this->title;
-	}
+                        		return $this->title;
+                        	}
 	
 	/**
 	 * @param string $title 
 	 * @return self
 	 */
 	public function setTitle(string $title): self {
-		$this->title = $title;
-		return $this;
-	}
+                        		$this->title = $title;
+                        		return $this;
+                        	}
 	
 	/**
 	 * @return string
 	 */
 	public function getContent(): string {
-		return $this->content;
-	}
+                        		return $this->content;
+                        	}
 	
 	/**
 	 * @param string $content 
 	 * @return self
 	 */
 	public function setContent(string $content): self {
-		$this->content = $content;
-		return $this;
-	}
+                        		$this->content = $content;
+                        		return $this;
+                        	}
 	
 	/**
 	 * @return \DateTime|null
 	 */
 	public function getAskedAt(): ?\DateTime {
-		return $this->askedAt;
-	}
+                        		return $this->askedAt;
+                        	}
 	
 	/**
 	 * @param \DateTime|null $askedAt 
 	 * @return self
 	 */
 	public function setAskedAt(?\DateTime $askedAt): self {
-		$this->askedAt = $askedAt;
-		return $this;
-	}
+                        		$this->askedAt = $askedAt;
+                        		return $this;
+                        	}
+
+    /**
+     * @return Collection<int, Answer>
+     */
+    public function getAnswers(): Collection
+    {
+        return $this->answers;
+    }
+
+    public function addAnswer(Answer $answer): self
+    {
+        if (!$this->answers->contains($answer)) {
+            $this->answers->add($answer);
+            $answer->setQuestion($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAnswer(Answer $answer): self
+    {
+        if ($this->answers->removeElement($answer)) {
+            // set the owning side to null (unless already changed)
+            if ($answer->getQuestion() === $this) {
+                $answer->setQuestion(null);
+            }
+        }
+
+        return $this;
+    }
 }
